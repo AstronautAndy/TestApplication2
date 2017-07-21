6958907f183368acbd454e745616f559095a13a7
TWG Help Desk <HelpDesk@weston>
2017-07-21 12:26:52 -0400

    Made some minor changes to the MainActivity.java class to experiment with dynamic UIs

--- app/src/main/java/com/example/helpdesk/testapplication2/MainActivity.java
+++ app/src/main/java/com/example/helpdesk/testapplication2/MainActivity.java
@@ -1,22 +1,159 @@
 package com.example.helpdesk.testapplication2;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
 
 import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+    List <Button> buttons; //Should be the set of buttons that can appear on the activity screen
+    LinearLayout buttonLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        buttons = new List<Button>() {
+            @Override
+            public int size() {
+                return 0;
+            }
+
+            @Override
+            public boolean isEmpty() {
+                return false;
+            }
+
+            @Override
+            public boolean contains(Object o) {
+                return false;
+            }
+
+            @NonNull
+            @Override
+            public Iterator<Button> iterator() {
+                return null;
+            }
+
+            @NonNull
+            @Override
+            public Object[] toArray() {
+                return new Object[0];
+            }
+
+            @NonNull
+            @Override
+            public <T> T[] toArray(@NonNull T[] a) {
+                return null;
+            }
+
+            @Override
+            public boolean add(Button button) {
+                return false;
+            }
+
+            @Override
+            public boolean remove(Object o) {
+                return false;
+            }
+
+            @Override
+            public boolean containsAll(@NonNull Collection<?> c) {
+                return false;
+            }
+
+            @Override
+            public boolean addAll(@NonNull Collection<? extends Button> c) {
+                return false;
+            }
+
+            @Override
+            public boolean addAll(int index, @NonNull Collection<? extends Button> c) {
+                return false;
+            }
+
+            @Override
+            public boolean removeAll(@NonNull Collection<?> c) {
+                return false;
+            }
+
+            @Override
+            public boolean retainAll(@NonNull Collection<?> c) {
+                return false;
+            }
+
+            @Override
+            public void clear() {
+
+            }
+
+            @Override
+            public Button get(int index) {
+                return null;
+            }
+
+            @Override
+            public Button set(int index, Button element) {
+                return null;
+            }
+
+            @Override
+            public void add(int index, Button element) {
+
+            }
+
+            @Override
+            public Button remove(int index) {
+                return null;
+            }
+
+            @Override
+            public int indexOf(Object o) {
+                return 0;
+            }
+
+            @Override
+            public int lastIndexOf(Object o) {
+                return 0;
+            }
+
+            @Override
+            public ListIterator<Button> listIterator() {
+                return null;
+            }
+
+            @NonNull
+            @Override
+            public ListIterator<Button> listIterator(int index) {
+                return null;
+            }
+
+            @NonNull
+            @Override
+            public List<Button> subList(int fromIndex, int toIndex) {
+                return null;
+            }
+        };
+        for(int i=0; i<3; i++){
+            Button newButton = new Button( getApplicationContext() );
+            buttons.add(newButton);
+            buttonLayout.addView(newButton);
+        }
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
